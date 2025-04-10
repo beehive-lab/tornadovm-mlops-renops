f222b49d3dbf1c532bbaabe66b23b5d90681edc0
Thanos Stratikopoulos <athanasios.stratikopoulos@manchester.ac.uk>
2025-04-10 15:17:00 +0300

    Added root variable

--- tornadovm-training/install.sh
+++ tornadovm-training/install.sh
@@ -2,4 +2,4 @@
 python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip setuptools wheel
-pip install -r requirements.txt
+pip install -r $TORNADOVM_MLOPS_ROOT/tornadovm-training/requirements.txt
